b06c8cd7522f5e4f0b2d62d22b0633ce2a1ae4e7
tanveerprottoy <tanveer.prottoy@gmail.com>
2022-07-13 16:30:23 +0600

    changes

--- src/utils/app.utils.ts
+++ src/utils/app.utils.ts
@@ -13,13 +13,4 @@ export class AppUtils {
     static dotenvConfig() {
         dotenv.config();
     }
-
-    static provideS3Vars(): string[] {
-        return [
-            process.env.S3_REGION,
-            process.env.S3_ACCESS_KEY_ID,
-            process.env.S3_SECRET_ACCESS_KEY,
-            process.env.S3_BUCKET_NAME,
-        ];
-    }
 }
